afe0a20c0f2c24e7f7d7c320d5ef96956ed0a576
Dzmitry Sharko <viplance.com@gmail.com>
2026-03-17 13:35:55 +0300

    Initial Commit

---
+++ FalconShot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FalconShot
+//
+//  Created by Dzmitry Sharko on 17.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FalconShot/FalconShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalconShotApp.swift
+//  FalconShot
+//
+//  Created by Dzmitry Sharko on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FalconShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
